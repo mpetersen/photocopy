1f8448579d33ce7d683a65c56313990d07fea899
Moritz Petersen <mpeterse@adobe.com>
2022-10-17 14:21:00 +0200

    Working on app. Better app icon.

--- artwork/makeicons.sh
+++ artwork/makeicons.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-SCALE=38
-BORDER=2
+SCALE=36
+BORDER=4
 TARGET="../src/main/resources/icons"
 
 mkdir -p $TARGET
-convert icon2.png -scale $SCALEx$SCALE -bordercolor none -border $BORDERx$BORDER $TARGET/menubar.png
-convert icon2.png -scale $SCALEx$SCALE -bordercolor none -border $BORDERx$BORDER -channel RGB -negate $TARGET/menubar_dark.png
+convert menubar_icon.png -scale $SCALEx$SCALE -bordercolor none -border $BORDERx$BORDER $TARGET/menubar.png
+convert menubar_icon.png -scale $SCALEx$SCALE -bordercolor none -border $BORDERx$BORDER -channel RGB -negate $TARGET/menubar_dark.png
 makeicns -in icon.png
